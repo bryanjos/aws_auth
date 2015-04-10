33f5db092d996890c5535460824b9d539c4899c0
Bryan Joseph <bryanjos@gmail.com>
2015-04-09 19:38:15 -0500

    Updated to 0.2.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule AWSAuth.Mixfile do
 
   def project do
     [app: :aws_auth,
-     version: "0.2.2",
+     version: "0.2.3",
      elixir: "~> 1.0",
      description: description,
      package: package,
