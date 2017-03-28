503f52ca25abc84aab51d7c91ec99cc86016bcfe
Ryan Bigg <git@ryanbigg.com>
2017-03-28 14:14:19 +1100

    Bump to 0.6.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule AWSAuth.Mixfile do
 
   def project do
     [app: :aws_auth,
-     version: "0.6.1",
+     version: "0.6.3",
      elixir: "~> 1.3",
      description: description(),
      package: package(),
